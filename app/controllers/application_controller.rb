638f1ffc30c7628e6fefa86bd2ded9ba569514af
川口耀平 <kawaguchi@kawaguchinoMacBook-Pro.local>
2021-11-04 16:28:56 +0900

    [update]#5 ユーザ登録画面にユーザーネームの入力フォームを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   respond_to :html, :json
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
