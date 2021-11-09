ad0641334efc5f22626a95179d7c9ae7e8b9102b
川口耀平 <kawaguchi@kawaguchinoMacBook-Pro.local>
2021-11-09 23:24:45 +0900

    [add]#26 レビュー詳細画面にて画像を表示し、拡大画像が閲覧できるモーダルを追加

---
+++ app/assets/javascripts/modal.js
@@ -0,0 +1,15 @@
+$(document).on('turbolinks:load', function () {
+  $('.review-image li').click(function() {
+    var imgSrc = $(this).children().attr('src');
+    $('.bigimg').children().attr('src', imgSrc);
+    $('.modal').fadeIn();
+    $('body,html').css('overflow-y', 'hidden');
+    return false
+  });
+
+  $('.close-btn').click(function() {
+    $('.modal').fadeOut();
+    $('body,html').css('overflow-y', 'visible');
+    return false
+  });
+});
