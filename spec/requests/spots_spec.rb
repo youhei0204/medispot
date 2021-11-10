10fce120f2d4e2a5dd6cea3e3bdf25faf241ebd2
川口耀平 <kawaguchi@kawaguchinoMacBook-Pro.local>
2021-11-11 00:17:02 +0900

    [add]#34 specを追加

--- spec/requests/spots_spec.rb
+++ spec/requests/spots_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Spot', type: :request do
     sign_in user
   end
 
-  describe 'GET /spots' do
+  describe 'GET /spots:id' do
     before { get spot_path spot }
 
     it 'スポット詳細画面の表示に成功すること' do
@@ -20,6 +20,92 @@ RSpec.describe 'Spot', type: :request do
       expect(response.body).to include spot.name
       expect(response.body).to include spot.address
       expect(response.body).to include review.title
+    end
+  end
+
+  describe 'GET /spots' do
+    before do
+      create_list(:spot, 5, :tokyo).each do |spot|
+        create(:review, :user, spot_id: spot.id, title: 'テストタイトル')
+      end
+      Spot.last.update!(address: "京都")
+    end
+
+    context 'エリアのみで検索し、検索結果が存在するとき' do
+      before { get spots_path, params: { area: "東京", keyword: "" } }
+
+      it 'スポット一覧画面の表示に成功すること' do
+        expect(response).to have_http_status(:success)
+      end
+      it '検索結果のスポット一覧が表示されること' do
+        expect(response.body).to include "東京都"
+        expect(response.body).to include "検索結果：4件"
+      end
+    end
+
+    context 'キーワードのみで検索し、検索結果が存在するとき' do
+      before { get spots_path, params: { area: "", keyword: "トーキョー" } }
+
+      it 'スポット一覧画面の表示に成功すること' do
+        expect(response).to have_http_status(:success)
+      end
+      it '検索結果のスポット一覧が表示されること' do
+        expect(response.body).to include "東京都"
+        expect(response.body).to include "検索結果：5件"
+      end
+    end
+
+    context 'エリア・キーワード両方で検索し、検索結果が存在するとき' do
+      before { get spots_path, params: { area: "東京", keyword: "テストタイトル" } }
+
+      it 'スポット一覧画面の表示に成功すること' do
+        expect(response).to have_http_status(:success)
+      end
+      it '検索結果のスポット一覧が表示されること' do
+        expect(response.body).to include "東京都"
+        expect(response.body).to include "検索結果：4件"
+      end
+    end
+
+    context '検索条件が空のとき' do
+      before do
+        get spots_path, params: { area: "", keyword: "" },
+                        headers: { "HTTP_REFERER": "http://example.com/home" }
+      end
+
+      it '遷移元画面の表示に成功すること' do
+        expect(response.status).to eq 302
+        expect(response).to redirect_to('http://example.com/home')
+      end
+    end
+
+    context '検索結果が0件のとき' do
+      before { get spots_path, params: { area: "ニューヨーク", keyword: "" } }
+
+      it 'スポット一覧画面の表示に成功すること' do
+        expect(response).to have_http_status(:success)
+      end
+      it '検索結果のスポット一覧が表示されないこと' do
+        expect(response.body).not_to include "東京"
+        expect(response.body).to include "検索結果：0件"
+      end
+    end
+
+    context '検索結果が100件を超えるとき' do
+      before do
+        create_list(:spot, 101, address: "ニューヨーク").each do |spot|
+          create(:review, :user, spot_id: spot.id)
+        end
+        get spots_path, params: { area: "ニューヨーク", keyword: "" }
+      end
+
+      it 'スポット一覧画面の表示に成功すること' do
+        expect(response).to have_http_status(:success)
+      end
+      it '検索結果のスポット一覧が100件まで表示されること' do
+        expect(response.body).to include "ニューヨーク"
+        expect(response.body).to include "検索結果：100件"
+      end
     end
   end
 end
---
+++ spec/system/spots_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Spot', type: :system do
+  let(:user) { create(:user) }
+  let(:spot) { create(:spot, :tokyo) }
+  let!(:review) { create(:review, :user, spot_id: spot.id, title: 'テストタイトル') }
+
+  before do
+    user.confirm
+    sign_in_as(user)
+  end
+
+  describe 'スポット検索' do
+    context '検索条件に合うスポットが存在するとき' do
+      before do
+        visit user_path user
+        fill_in 'header-area', with: '東京'
+        fill_in 'header-keyword', with: 'テストタイトル'
+        click_button '検索'
+      end
+
+      it 'スポット詳細画面に検索結果が表示される' do
+        expect(current_path).to eq spots_path
+        expect(page).to have_content '検索結果：1件'
+        within '.spot-box' do
+          expect(page).to have_content 'トーキョー'
+          expect(page).to have_content '東京都'
+          expect(page).to have_content 3.0
+        end
+      end
+    end
+
+    context '検索条件に合うスポットが存在しないとき' do
+      before do
+        visit user_path user
+        fill_in 'header-area', with: 'ニューヨーク'
+        fill_in 'header-keyword', with: 'テストタイトル'
+        click_button '検索'
+      end
+
+      it 'スポット詳細画面に検索結果が表示されないこと' do
+        expect(current_path).to eq spots_path
+        expect(page).to have_content '検索結果：0件'
+        boxes = all(:css, 'spot-box')
+        expect(boxes.count).to eq 0
+      end
+    end
+
+    context '条件を入力せずに検索するとき' do
+      before do
+        visit user_path user
+        fill_in 'header-area', with: ''
+        fill_in 'header-keyword', with: ''
+        click_button '検索'
+      end
+
+      it '遷移元画面にリダイレクトすること' do
+        expect(current_path).to eq(user_path(user))
+      end
+    end
+  end
+end
