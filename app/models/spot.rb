193c07523d2c8ade155cb60edc54964459f0a3ca
川口耀平 <kawaguchi@kawaguchinoMacBook-Pro.local>
2021-11-11 12:09:41 +0900

    [add]#36 Favoriteモデルを追加

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,5 +1,6 @@
 class Spot < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   validates :name, presence: true, length: { maximum: 100 }
   validates :address, presence: true, length: { maximum: 200 }
   validates :lat, presence: true
