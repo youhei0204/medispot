d80c2c186e4eb0d13923202801f0335f74a6119b
川口耀平 <kawaguchi@kawaguchinoMacBook-Pro.local>
2021-11-11 12:14:21 +0900

    [add]#36 プロフィール画面に保存済みスポットの一覧を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   has_one_attached :image
   has_many :reviews, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: true, length: { maximum: 30 }
