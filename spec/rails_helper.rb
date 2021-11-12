08eadd5f70e59e4b6ae2d44cf8def31046090a9e
川口耀平 <kawaguchi@kawaguchinoMacBook-Pro.local>
2021-11-12 14:16:03 +0900

    [add] database cleanerを追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,17 @@ Capybara.register_driver :remote_chrome do |app|
 end
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
