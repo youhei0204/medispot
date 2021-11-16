fb9b3afd2dd22aa51e25b21a52c00583bed07d48
川口耀平 <kawaguchi@kawaguchinoMacBook-Pro.local>
2021-11-16 20:39:36 +0900

    [update]レビューの星評価のバリデーションエラーメッセージを変更
    星評価は星マークをクリックすることで数値入力するため、実際の入力エラーに合わせて以下の文言に変更
    星評価は数値で入力してください -> 星評価を選択してください

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,6 +7,7 @@ class Review < ApplicationRecord
   validates :content, presence: true, length: { maximum: 2000 }
   validates :rate, numericality: {
     greater_than_or_equal_to: 0,
-    less_than_or_equal_to: 5.0
+    less_than_or_equal_to: 5.0,
+    message: 'を選択してください'
   }
 end
