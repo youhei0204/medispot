5cd2cf9ec87bfce56c0ad5f9e63059fab154a1b6
川口耀平 <kawaguchi@kawaguchinoMacBook-Pro.local>
2021-11-21 02:00:32 +0900

    [add]#69 レビューをLIKEしたときに新着通知を作成する処理を追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,35 @@
 class LikesController < ApplicationController
   def create
-    current_user.likes.create(review_id: params[:review_id])
+    if current_user.likes.create(review_id: params[:review_id])
+      update_notification
+    end
   end
 
   def destroy
     like = current_user.likes.find_by(review_id: params[:review_id])
     like.destroy if like.present?
+  end
+
+  private
+
+  def update_notification
+    reviewer = Review.find_by(id: params[:review_id]).user
+    if current_user != reviewer
+      reviewer.notifications.
+        find_by(
+          sender_id: current_user.id,
+          request_type: 1,
+          link_id: params[:review_id]
+        )&.destroy
+
+      reviewer.notifications.create(
+        sender_id: current_user.id,
+        sender_name: current_user.name,
+        request_type: 1,
+        subject: Review.find_by(id: params[:review_id]).title,
+        link_id: params[:review_id],
+      )
+      reviewer.update(new_notification_flag: true)
+    end
   end
 end
