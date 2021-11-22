f1100bd0e56684122d81a594e3fbd9085050e6c0
川口耀平 <kawaguchi@kawaguchinoMacBook-Pro.local>
2021-11-22 16:40:16 +0900

    [update]#73 通知ボックスにAboutページ用の通知が表示されるよう改修

--- app/assets/javascripts/notification_accordion.js
+++ app/assets/javascripts/notification_accordion.js
@@ -1,8 +1,8 @@
 $(document).on('turbolinks:load', function () {
-    $(document).click(function(event){
-      var target = $(event.target);
-      if(target.attr('id') != 'notification' && target.not('.notification-box')) {
-        $('.notification-box').remove();
-      }
-    });
+  $(document).click(function(event){
+    var target = $(event.target);
+    if(target.attr('id') != 'notification' && target.not('.notification-box')) {
+      $('.notification-box').remove();
+    }
+  });
 });
