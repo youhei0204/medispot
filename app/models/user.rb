bdadf07830f2b0728fa3a53dc29310bf05b9e1d0
川口耀平 <kawaguchi@kawaguchinoMacBook-Pro.local>
2021-11-23 16:09:33 +0900

    [update]#75 ゲストユーザーの数を1->5に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  GUEST_USER_NUM = 5
   has_one_attached :image
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
@@ -13,11 +14,25 @@ class User < ApplicationRecord
   validates :new_notification_flag, inclusion: { in: [true, false] }
 
   def self.guest
-    find_or_create_by!(email: 'guest@medispot.com') do |user|
-      user.name = 'ゲスト'
-      user.introduction = 'ゲストとしてログインしています。'
-      user.password = SecureRandom.urlsafe_base64
-      user.confirmed_at = Time.now
+    colors = ['red', 'blue', 'yellow', 'purple', 'green']
+    colors_to_jp = { 'red' => '赤', 'blue' => '青', 'yellow' => '黄', 'purple' => '紫', 'green' => '緑' }
+
+    guest_users = User.where("email like 'guest_%@medispot.com'").where.not(confirmed_at: nil)
+    if guest_users.count == GUEST_USER_NUM
+      guest_users.joins(:notifications).order('notifications.created_at').first
+    else
+      colors.each do |color|
+        if !User.find_by(email: "guest_#{color}@medispot.com")
+          user = User.create(
+            name: "ゲスト(#{colors_to_jp[color]})",
+            email: "guest_#{color}@medispot.com",
+            introduction: "ゲスト(#{colors_to_jp[color]})としてログインしています。",
+            password: SecureRandom.urlsafe_base64,
+            confirmed_at: Time.now
+          )
+          return user
+        end
+      end
     end
   end
 end
